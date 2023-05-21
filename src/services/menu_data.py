0ec690091905f2719c9dd0011468e59940b969e1
RafaelRRhocha <1rafaelrocha2@gmail.com>
2023-05-21 18:05:14 -0300

    feat add req 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,24 @@
+import pandas as pd
+from models.dish import Dish
+from models.ingredient import Ingredient
+
+
 # Req 3
 class MenuData:
-    def __init__(self, source_path: str) -> None:
-        pass
+    def __init__(self, source_path: str):
+        self.csv_reader = pd.read_csv(source_path).itertuples(index=False)
+        self.dishes = set()
+
+        obj = {}
+
+        for dish_info in csv_reader:
+            price, name, amount, ingredient_name = dish_info
+            if name not in obj:
+                dish = Dish(name, price)
+                obj[name] = dish
+                self.dishes.add(dish)
+            ingredient_obj = Ingredient(ingredient_name)
+            obj[name].add_ingredient_dependency(ingredient_obj, amount)
+
+    def __len__(self):
+        return len(self.dishes)
