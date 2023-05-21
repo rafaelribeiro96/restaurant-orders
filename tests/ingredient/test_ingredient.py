641b65f58398c0f934c8028fabebf0a83a05da46
RafaelRRhocha <1rafaelrocha2@gmail.com>
2023-05-21 17:56:23 -0300

    feat: add req 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,7 +1,23 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
 
 
 # Req 1
-# iniciando projeto
 def test_ingredient():
-    pass
+    bacon = "bacon"
+    shrimp = "camarão"
+
+    ingredient_shrimp = Ingredient(shrimp)
+    ingredient_bacon = Ingredient(bacon)
+    ingredient_bacon_two = Ingredient(bacon)
+
+    assert ingredient_bacon.name == bacon
+    assert repr(ingredient_bacon) == f"Ingredient('{bacon}')"
+
+    assert hash(ingredient_bacon) == hash(ingredient_bacon_two)
+    assert hash(ingredient_bacon) != hash(ingredient_shrimp)
+
+    assert ingredient_bacon.__eq__(ingredient_bacon_two) is True
+    assert ingredient_bacon.__eq__(ingredient_shrimp) is False
+
+    bacon_restrictions = {Restriction.ANIMAL_MEAT, Restriction.ANIMAL_DERIVED}
+    assert ingredient_bacon.restrictions == bacon_restrictions
